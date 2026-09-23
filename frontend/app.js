58749aec1ccf472a6fc7ce396bb22a86a2bdf99b
Pyro <priyanthan@web.de>
2026-09-23 16:12:53 +0200

    feat(frontend): add second page route

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,7 @@
 function greetUser(name) {
     return `Hello, ${name}! Welcome to our application built with Terraform and Kubernetes.`;
 }
+
+function greetUser2(name) {
+    return `Hello, ${name}! Welcome to our application built with Terraform and Kubernetes V2`;
+}
